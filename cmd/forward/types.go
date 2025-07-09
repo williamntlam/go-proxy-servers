5454733973c3449fbc724d7fa54c2758b5df3240
William Lam <williamntlam@gmail.com>
2025-07-09 18:55:09 -0400

    fix: fixed syntax issues; proxy server now starts

--- cmd/forward/types.go
+++ cmd/forward/types.go
@@ -1,3 +1,13 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
 type ProxyHandler struct{}
+
+func (h *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Fprintf(w, "Proxy server is running!")
+}
